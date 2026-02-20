26908c75f537ce2a872e4ecd9922e2c36d572768
Andrea <andy.gpe.sm@gmail.com>
2026-02-19 19:19:05 -0700

    correciones

--- src/main.py
+++ src/main.py
@@ -4,13 +4,15 @@ import math
 class Calculator:
 
     def sum(self, a: int, b: int) -> int:
-        return a - b  
+        return a + b  
 
     def restar(self, a: int, b: int) -> int:
-        return a + b  
+        return a - b  
 
     def multiply(self, a: int, b: int) -> int:
-        return a + b  
+        return a * b  
 
     def divide(self, a: int, b: int) -> float:
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
         return a / b  
